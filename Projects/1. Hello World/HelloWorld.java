aa319ef7a1bda12196259557ad9fe3d92f46c0e4
Brailinson Disla <Brailinson.Disla@gmail.com>
2021-01-07 13:07:33 -0500

    Update -added HelloWorld

---
+++ Projects/1. Hello World/HelloWorld.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *  Name: Brailinson Disla
+ *    - alias: 0xffbraid
+ *
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World" to standard output device -- terminal.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ ******************************************************************************/
+public class HelloWorld {
+    public static void main(String[] args) {
+		    System.out.println("Hello, World");
+    }
+}
+
+/******************************************************************************
+ *  Notes:
+ *    - the class name must be the same as the file name (w/out the extension)
+ *    - the entry point function is main, and the method signature must be:
+ *      + public static void main(String[] args)
+ *          - args can be renamed to any valid identifier
+ *      + return type is void, nothing is to be returned
+ ******************************************************************************/
